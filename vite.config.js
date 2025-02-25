896c92cc989bd47942b9e90ddd52b8c63a5343a0
Ruth Sotnik <ruthsotnik@MacBook-Pro-2.local>
2025-02-25 18:26:03 +0200

    Vite base corrected

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/e-plantShopping/', // 👈 Set the base to match your GitHub Pages repo name
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,3 +11,4 @@ export default defineConfig({
     },
   },
 });
+
